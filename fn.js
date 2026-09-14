15523c00419d536c448f031f11cdfd32ee61bd31
yashraj <yashrajb916@gmail.com>
2026-09-14 07:04:56 +0530

    uppercase letter accept functionality

--- fn.js
+++ fn.js
@@ -7,7 +7,7 @@ searchBox.addEventListener('keypress',async(e)=>{
         const response = await fetch("file.json");
         const data = await response.json();
         const updateList = data.filter((recipe)=>{
-            return recipe.RecipeName.includes(searchBox.value);
+            return recipe.RecipeName.includes(searchBox.value.toLowerCase());
         })
         recipeContainer.innerHTML = "";
         if(updateList.length ===0){
