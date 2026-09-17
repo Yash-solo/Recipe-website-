7e966cacf4ff43ac4b923b273bb27d8b38a47ba6
yashraj <yashrajb916@gmail.com>
2026-09-17 11:22:26 +0530

    Change file path

--- otherPages/Process.js
+++ otherPages/Process.js
@@ -13,14 +13,17 @@ async function getRecipes(){
     //fetch recipes when you need to show recipe
     const response = await fetch("/file.json");
     const data = await response.json();
-    //find the recipe user wants 
-    Recipe = data.find(item=> item.id===Number(id))
-
-    //render recipe's process,ingredients,pic and name
-    recipeHeading.textContent = Recipe.RecipeName;
-    console.log(Recipe.RecipePic)
-    recipeImage.src = `/${Recipe.RecipePic}`;
-    recipeProcess.textContent = Recipe.RecipeMessage;
+    if(response.ok){
+        //find the recipe user wants 
+        const Recipe = data.find(item=> item.id===Number(id))
+    
+        //render recipe's process,ingredients,pic and name
+        recipeHeading.textContent = Recipe.RecipeName;
+        console.log(Recipe.RecipePic)
+        recipeImage.src = `/${Recipe.RecipePic}`;
+        recipeProcess.textContent = Recipe.RecipeMessage;
+    }
+    alert("wrong path of file.json")
 }
 //call the funtion imidiatly
 getRecipes();
