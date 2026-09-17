534dc1bf528e06c3f5e220c20f132d3c3333b5af
Yas-prog-blip <yashrajb916@gmail.com>
2026-09-17 11:40:38 +0530

    Update Process.js

--- otherPages/Process.js
+++ otherPages/Process.js
@@ -11,7 +11,7 @@ const recipeProcess = document.querySelector('.RecipeProcess');
 
 async function getRecipes(){
     //fetch recipes when you need to show recipe
-    const response = await fetch("./file.json");
+    const response = await fetch("../file.json");
     
     const data = await response.json();
     if(response.ok){
