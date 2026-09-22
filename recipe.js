b21e83339ad780fc296c0b86f5f4a14f48186ebb
yashraj <yashrajb916@gmail.com>
2026-09-22 07:07:44 +0530

    Add navbar

--- recipe.js
+++ recipe.js
@@ -1,29 +1,32 @@
+const menubar = document.querySelector(".menubar");
+let isOpen = false;
+const upperLine = document.querySelector(".upperline");
+const midLine   = document.querySelector('.Midline');
+const lowerLine = document.querySelector('.Lowerline');
+const sidebar = document.querySelector('.sidebar')
 
-function showSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex';
-}
-function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none';
-}
-function show_malpuaa() {
-    window.location.href = "otherPages/malpua_process.html";
 
-}
-function show_guliapa() {
-    window.location.href = 'otherPages/guliapa_process.html';
+menubar.addEventListener('click',(e)=>{
+    if(isOpen === false){
+        midLine.style.display = "none";
+        upperLine.classList.add("rotate-[405deg]","translate-y-2")
+        lowerLine.classList.add("-rotate-[405deg]","-translate-y-0.5")
+        isOpen=true;
+    }else{
+        midLine.style.display = "flex";
+        upperLine.classList.remove("rotate-[405deg]","translate-y-2")
+        lowerLine.classList.remove("-rotate-[405deg]","-translate-y-0.5")
+        isOpen = false;
+    }
+    if(isOpen===false){
+        sidebar.style.display = "none";
+        // sidebar.classList.remove('right-0');
+        // sidebar.classList.add("relative","-right-full")
+    }else{
+        sidebar.style.display = "flex";
 
-}
-function show_wuf() {
-    window.location.href = 'otherPages/wuf_process.html';
-}
-function Show_chana() {
-    window.location.href = "otherPages/chana_process.html";
-}
-function show_quick_chana() {
-    window.location.href = 'otherPages/quick_chana_process.html';
-}
-function show_pasta() {
-    window.location.href = 'otherPages/pasta_process.html';
-}
+        // sidebar.classList.remove("-right-full");
+        // sidebar.classList.add('right-0',"relative")
+    }
+
+})
