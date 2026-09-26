934a8fcff9c15935f8082a11968c2ae710b98787
yashraj <yashrajb916@gmail.com>
2026-09-26 06:53:45 +0530

    Add fav

--- recipe.js
+++ recipe.js
@@ -8,11 +8,6 @@ function showcategories(){
 }
 function showFaviorate(){
     mainContent.innerHTML = `
-    <label style="padding:1px 4px 1px 4px"class="w-10 shadow-[0px_0px_5px_rgba(0,0,0,0.3)] rounded-[50%] border border-gray-300 items-center justify-around inline-flex">
-        <input type="checkbox" class="peer sr-only"/>
-        <span class="text-4xl text-gray-300
-            peer-checked:text-red-500
-        ">♥</span>
-    </label>`
+    <div style="padding:12px;"class="w-full  h-full bg-amber-500">Hello</div>`
     
 }
